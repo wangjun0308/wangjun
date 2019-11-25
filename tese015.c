d26ab8c885d108d5644eeda3242847994dfb15cb
wangjun0308 <1912067613@qq.com>
2019-11-25 09:38:40 +0800

    0

--- tese015.c
+++ tese015.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+#include<math.h>
 int main(void)
 {
     int n,i;
-    printf("Enter n:\n");
-    scanf("%d",&n);
+    for(n=2;n<=500;n++)
+    {
     for(i=2;i<n;i++)
     {
         if(n%i==0)
@@ -19,6 +20,6 @@ int main(void)
     {
         printf("no!\n");
     }
-    
+    }
     return 0;
 }
