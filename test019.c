25886cf78067dffa90c592dfebe75d8f50f070e0
wangjun0308 <1912067613@qq.com>
2019-12-10 17:28:00 +0800

    0

--- test019.c
+++ test019.c
@@ -2,16 +2,20 @@
 #define len 5
 int main(void)
 {
-    int a[len], i;
+    int a[len], i,max;
     printf("Enter array a:\n");
     for (i = 0; i < len; i++)
     {
         scanf("%d", &a[i]);
     }
-    for (i = len-1; i >=0; i--)
+    max=a[0];
+    for (i = 1; i <len; i++)
     {
-        printf("%5d", a[i]);
+        if(a[i]>max){
+            max=a[i];
+        }
     }
+    printf("max is %d\n",max);
 
     return 0;
 }
