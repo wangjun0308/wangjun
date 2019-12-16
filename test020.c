f4e8e43821dc0e20d3628997ae26466c73ae7da3
wangjun0308 <1912067613@qq.com>
2019-12-16 08:47:19 +0800

    0

---
+++ test020.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define len 5
+int main(void)
+{
+    int a[len],max,i;
+    printf("Enter array a:\n");
+    for(i=0;i<len;i++){
+        scanf("%d",&a[i]);
+    }
+    max=findmax(a,len);
+    printf("%d",max);
+    return 0;
+}
+int findmax(int x[],int n)
+{
+    int m,i;
+    m=x[0];
+    for(i=1;i<n;i++){
+        if(x[i]>m){
+            m=x[i];
+        }
+    }
+    return m;
+}
